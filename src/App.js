39f7114c64aef997988aabc2778914ed5727c952
Adailson Santos <adailson2@gmail.com>
2018-05-11 18:53:28 -0300

    Configuring increment in temperature.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import Temperature from './Components/Temperature';
+import './style.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>GreenHouse</h1>
         <Temperature />
       </div>
     );
