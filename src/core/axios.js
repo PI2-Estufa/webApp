2e4c6c4553d82e92c41f6ea7b1d5bcac36b978d9
Lucas Amoêdo <lucas.advc@gmail.com>
2018-06-29 12:21:05 -0300

    Update endpoint

--- src/core/axios.js
+++ src/core/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const baseUrl = 'http://localhost:8000';
+export const baseUrl = process.env.NODE_ENV == 'development' ? 'http://localhost:8000' : 'http://52.67.63.21:8000';
 
 axios.defaults.baseURL = baseUrl;
 
